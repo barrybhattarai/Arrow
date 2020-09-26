ef72c1386d39457e5b6c793cc0bad54248993312
¨Barry <bhanubhattarai4@gmail.com>
2020-09-27 00:31:50 +0530

    comment for read_file function in util.rs

--- src/util.rs
+++ src/util.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{Read, Result};
 use std::path::Path;
 
+//read_file reads a file into a string
 pub fn read_file(path: &Path) -> Result<String> {
     match File::open(path) {
         Ok(mut f) => {
